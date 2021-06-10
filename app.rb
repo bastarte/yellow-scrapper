d43d614d83b0686cb7b860e464a5a2ec582659ec
Bastien Hubervic-Renard <bastienhr+github@gmail.com>
2021-06-10 11:05:21 +0200

    moved app.rb up for cleaner directory structure

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require 'open-uri'
 require 'nokogiri'
 require 'csv'
 
-require_relative 'scrapper'
-require_relative 'controller'
+require_relative 'app/scrapper'
+require_relative 'app/controller'
 
 controller = Controller.new
 controller.import_from_web
